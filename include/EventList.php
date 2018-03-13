e676dd63efa98681831fe4a3d84570e32138e9de
bahrozz <you@example.com>
2018-03-14 00:14:25 +0300

    add event list & image

---
+++ include/EventList.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+	
+	
+	</style>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap-min.css">
+
+	<link rel="stylesheet" type="text/css" href="../css/eventliststyle.css">
+	<title>Event List</title>
+</head>
+<body>
+<?php include ('Header.php');?>
+	<div class="container" style="width: 100%; height: 300px; ">
+		<img src="../images/maxresdefault.jpg">
+		</div>
+		<div class="container" style="width: 100%; height: 150px; text-align: center; font-size: 30px; font-weight: bolder;"> Event Name</div>
+		<div class="container"><form>
+			<div class="input-group">
+				<input type="text" class="form-control" placeholder="Search For Event" name="serach">
+				<div class="input-group-btn"><button type="button" class="btn btn-info">
+      <span class="glyphicon glyphicon-search"></span> Search
+    </button></div>
+			</div>
+		</form></div>
+<div class="row container1" style="padding: 5px; width: 100%;">
+    <div class="imageBox col-sm-4" ><img src="../images/cinama.png">
+   
+   <a href="yourticket.php"><div class="textBox"><h1>cinama festival</h1>
+   </div></a>
+</div>
+    <div class="imageBox col-sm-4" ><img src="../images/music.jpg">
+   
+   <a href="yourticket.php"><div class="textBox"><h1>Music festival</h1>
+   </div></a>
+</div>
+    <div class="imageBox col-sm-4" ><img src="../images/dims.jpg">
+   
+   <a href="yourticket.php"><div class="textBox"><h1>Swimming festival</h1>
+   </div></div></a>
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="binar,bahroz,ardin,choman">
+    <link rel="icon" href="../../favicon.ico">
+    <title>MyEvents</title>
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+     <link rel="stylesheet" type="text/css" href="css/bootstrap-min.css">
+    <link href="bug-workaround.css" rel="stylesheet">
+    <link href="navbar.css" rel="stylesheet">
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  </head>
+
+  <body>
+
+   <?php include 'include/Header.php';  ?>
+   <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active" style="text-align:center;font-size:40px;font-weight: bold;color:white;">music festival</li>
+    <li data-target="#myCarousel" data-slide-to="1">nan festival</li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/bb.jpg" alt="Los Angeles" style="width:100%;height:500px;">
+    </div>
+
+    <div class="item">
+      <img src="images/bbb.jpg" alt="Chicago" style="width:100%;height:500px;">
+    </div>
+
+    <div class="item">
+      <img src="images/b.jpg" alt="New York" style="width:100%;height:500px;">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div  class="container ">
+	<h1>binar</h1>
+</div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    
+   
+  </body>
+</html>
