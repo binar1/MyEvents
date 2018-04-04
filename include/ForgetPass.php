a384a1936803e0443a28189d27c58966865af379
bahrozz <you@example.com>
2018-04-04 15:25:04 +0300

    add Profile Organization

--- include/ForgetPass.php
+++ include/ForgetPass.php
@@ -3,7 +3,12 @@
 <head>
 	<title>Forget Password</title>
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap-min.css">
-	<style type="text/css">
+	
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style type="text/css">
+
 		.center {
     		display: block;
     		margin-left: auto;
@@ -15,7 +20,7 @@
 			width: 50%;
 			height: 50%;
 			border-radius: 5px;
-			padding: 10px;
+			padding: 20px;
 		}
 			@font-face{
  			 font-family:Roboto;
@@ -39,19 +44,18 @@
 </head>
 <body style="background-color: #ebebe0;">
 <?php include 'Header.php'; ?>
-	<div style="padding: 60px;"></div>
-	<div class="container center bgbox">
-		<h3 align="center">Forget Password</h3>
-
+	<div style="padding: 60px; "></div>
+	<div class="container center bgbox" style="width: 80%;">
+		<h3 align="center" >Forget Password</h3>
 		<div class="input-group" style="padding: 10px;">
 			<span class="input-group-addon">Email</span>
-			<input id="email" type="text" class="form-control" name="email" placeholder="Write Your Email">
-		</div>
-   	    <input type="submit" name="submit" value="send" class="btn btn-danger center" >
-		
-		
-	</div>
-
+			<input id="email" type="text" class="form-control" name="email" placeholder="Write Your Email" >
+		</div >
+			<div>
+   	    		<input type="submit" name="submit" value="send" class="btn btn-danger center" >
+   	    	</div>
+   </div>
+	<div style="padding-bottom: 100px;"></div>
 <?php include 'Footer.php'; ?>
 
 </body>
